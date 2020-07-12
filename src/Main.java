51b6d1486b9212fd064057b5b2fb49fe4618cfc8
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2020-07-12 22:05:32 +0200

    implemented FFT-transformation of filtered signal

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,27 @@ public class Main {
         // Aufgabe 3
         Bandpass filter = new Bandpass(4, 10, 1000, sampleRate);
         double[] filteredSignal = signal.clone();
-        Arrays.stream(filteredSignal).forEachOrdered(filter::step);
+        for (int i = 0; i < filteredSignal.length; i++) {
+            filteredSignal[i] = filter.step(filteredSignal[i]);
+        }
+
+        // Aufgabe 4
+        //find next power of 2
+        int power = 1;
+        while (power < filteredSignal.length) power *= 2;
+        Complex[] cFilteredSignal = new Complex[power];
+        Complex[] cSignal = new Complex[power];
+        for (int i = 0; i < cFilteredSignal.length; i++) {
+            cFilteredSignal[i] = (i < filteredSignal.length) ? new Complex(filteredSignal[i],0) : new Complex(0,0);
+            cSignal[i] = (i < signal.length) ? new Complex(signal[i],0) : new Complex(0,0);
+
+        }
+
+        Complex[] filteredSpectrum = FFT.fft(cFilteredSignal);
+        Complex[] Spectrum = FFT.fft(cSignal);
+
+        for (int i = 0; i < filteredSpectrum.length; i++) {
+            System.out.println("Index:" + i + "\tOriginal:" + Spectrum[i].re() + "\tFiltered:" + filteredSpectrum[i].re());
+        }
     }
 }
