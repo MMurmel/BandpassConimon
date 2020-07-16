195f73267ed0ac2b8b8e3fb01fec0891a27bfaf4
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2020-07-16 16:21:45 +0200

    implemented task 5 not sure why spectrum peaks are shifted

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int sampleRate = 96000;
         //int sampleRate = Collections.max(Arrays.asList(frequencies)) * 2 + 1;
         double[] signal = SignalGenerator.generateSuperimposingSineWaves(Arrays.asList(frequencies), sampleRate);
-        dataToFile(Arrays.stream(signal).boxed().toArray(Double[]::new), "signal");
+        dataToFile(Arrays.stream(signal).boxed().toArray(Double[]::new), "signal", true);
 
         // Aufgabe 3
         Bandpass filter = new Bandpass(4, 10, 1000, sampleRate);
@@ -19,7 +19,7 @@ public class Main {
         for (int i = 0; i < filteredSignal.length; i++) {
             filteredSignal[i] = filter.step(filteredSignal[i]);
         }
-        dataToFile(Arrays.stream(filteredSignal).boxed().toArray(Double[]::new), "filtered");
+        dataToFile(Arrays.stream(filteredSignal).boxed().toArray(Double[]::new), "filtered", true);
 
         // Aufgabe 4
         //find next power of 2
@@ -37,37 +37,74 @@ public class Main {
         Complex[] spectrum = FFT.fft(cSignal);
         Complex[] filteredSpectrum = FFT.fft(cFilteredSignal);
 
-        dataToFile(Arrays.stream(spectrum).map(Complex::abs).toArray(Double[]::new), "spectrum");
-        dataToFile(Arrays.stream(filteredSpectrum).map(Complex::abs).toArray(Double[]::new), "filtered_spectrum");
+        int limitSpektrum = 20000;
+        dataToFile(Arrays.stream(spectrum).limit(limitSpektrum).map(Complex::abs).toArray(Double[]::new), "spectrum",false);
+        dataToFile(Arrays.stream(filteredSpectrum).limit(limitSpektrum).map(Complex::abs).toArray(Double[]::new), "filtered_spectrum", false);
+
+        // Aufgabe 5
+        // finding maximal values in the spectrum around the expected maxima
+        // for original signal and the filtered one
+        Double[] amplitudes = Arrays.stream(spectrum).map(Complex::abs).toArray(Double[]::new);
+        Double[] fAmplitudes = Arrays.stream(filteredSpectrum).map(Complex::abs).toArray(Double[]::new);
+
+        // using this complicated way instead of Collections.max(…), because the highest value in the unfiltered spectrum isn't necessarily the highest in the
+        int low = 0,flow = 0,mid = 100,fmid = 100,high = 10000,fhigh = 10000;
+        for (int i = 0; i < 10; i++) {
+            if (amplitudes[i] > amplitudes[low]) low = i;
+            if (fAmplitudes[i] > fAmplitudes[flow]) flow = i;
+        }
+
+        for (int i = 100; i < 120; i++) {
+            if (amplitudes[i] > amplitudes[mid]) mid = i;
+            if (fAmplitudes[i] > fAmplitudes[fmid]) fmid = i;
+        }
+
+        for (int i = 10000; i < 14000; i++) {
+            if (amplitudes[i] > amplitudes[high]) high = i;
+            if (fAmplitudes[i] > fAmplitudes[fhigh]) fhigh = i;
+        }
+
+        // calculating dampening
+        double dampLow = 10 * Math.log10(fAmplitudes[flow] / amplitudes[low]);
+        double dampMid = 10 * Math.log10(fAmplitudes[fmid] / amplitudes[mid]);
+        double dampHigh = 10 * Math.log10(fAmplitudes[fhigh] / amplitudes[high]);
+
+        System.out.println("Low frequency: value original:" + amplitudes[low] + " \tvalue filtered" + fAmplitudes[flow] + "\tdampening" + dampLow);
+        System.out.println("Mid frequency: value original:" + amplitudes[mid] + " \tvalue filtered" + fAmplitudes[fmid] + "\tdampening" + dampMid);
+        System.out.println("High frequency: value original:" + amplitudes[high] + " \tvalue filtered" + fAmplitudes[fhigh] + "\tdampening" + dampHigh);
+
 
         //--------------------------
         // Aufgabe 6 und 7
-        double[] noise = SignalGenerator.generateNoise(1, 96000);
-        dataToFile(Arrays.stream(noise).boxed().toArray(Double[]::new), "noise");
+        // generating noise
+        double[] noise = SignalGenerator.generateNoise(1, sampleRate);
+        dataToFile(Arrays.stream(noise).boxed().toArray(Double[]::new), "noise", true);
 
+        // filtering noise (analogous to signal filtering)
         double[] filteredNoise = noise.clone();
         for (int i = 0; i < filteredNoise.length; i++) {
             filteredNoise[i] = filter.step(filteredNoise[i]);
         }
-        dataToFile(Arrays.stream(filteredNoise).boxed().toArray(Double[]::new), "filtered_noise");
+        dataToFile(Arrays.stream(filteredNoise).boxed().toArray(Double[]::new), "filtered_noise",true);
 
+        // FFT for noise and filtered noise
         Complex[] cNoise = new Complex[power];
         Complex[] cFilteredNoise = new Complex[power];
         for (int i = 0; i < cFilteredSignal.length; i++) {
-            cNoise[i] = (i < signal.length) ? new Complex(noise[i],0) : new Complex(0,0);
-            cFilteredNoise[i] = (i < signal.length) ? new Complex(filteredNoise[i],0) : new Complex(0,0);
+            cNoise[i] = (i < noise.length) ? new Complex(noise[i],0) : new Complex(0,0);
+            cFilteredNoise[i] = (i < filteredNoise.length) ? new Complex(filteredNoise[i],0) : new Complex(0,0);
         }
 
         Complex[] noiseSpectrum = FFT.fft(cNoise);
         Complex[] filteredNoiseSpectrum = FFT.fft(cFilteredNoise);
 
-        dataToFile(Arrays.stream(noiseSpectrum).map(Complex::abs).toArray(Double[]::new), "noise_spectrum");
-        dataToFile(Arrays.stream(filteredNoiseSpectrum).map(Complex::abs).toArray(Double[]::new), "filtered_noise_spectrum");
+        dataToFile(Arrays.stream(noiseSpectrum).limit(limitSpektrum).map(Complex::abs).toArray(Double[]::new), "noise_spectrum", false);
+        dataToFile(Arrays.stream(filteredNoiseSpectrum).limit(limitSpektrum).map(Complex::abs).toArray(Double[]::new), "filtered_noise_spectrum", false);
 
     }
 
     // helper function to write data to a file (used for visualization)
-    protected static void dataToFile(Double[] data, String name) throws IOException {
+    protected static void dataToFile(Double[] data, String name, boolean scale) throws IOException {
         String fileName = "/home/murmel/IdeaProjects/BandpassConimon/graphics/" + name + ".data";
 
         new FileWriter(fileName, false).close();
@@ -76,7 +113,7 @@ public class Main {
 
         for (int i = 0; i < data.length; i++) {
             String s = data[i] + "\n";
-            bw.write(i + "    " + s);
+            bw.write((scale ? (double)i/data.length : i) + "    " + s);
         }
 
         bw.close();
